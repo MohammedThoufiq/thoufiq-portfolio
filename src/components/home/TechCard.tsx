ae8ae4e78114ef1e2f1b8e2369e585b30387d8e5
MohammedThoufiq <mohammedthoufiq25112000@gmail.com>
2026-06-13 17:26:47 +0530

    Enhancing the animation and adding the contact button in the hero section
    • Animating the border glow and created the magnetic animation for future use
    • Adding the contact button in the Hero Section

--- src/components/home/TechCard.tsx
+++ src/components/home/TechCard.tsx
@@ -1,5 +1,8 @@
-import Link from "next/link";
+"use client";
+
+//import Link from "next/link";
 import Image from "next/image";
+import BorderGlow from "../animations/BorderGlow";
 
 type Props = {
   name: string;
@@ -12,52 +15,57 @@ export default function TechCard({
   name,
   icon,
   category,
-  url,
+  //url,
 }: Props) {
   return (
-    <Link
-      href={url}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="
-        group
-        rounded-2xl
-        border border-white/10
-        bg-white/[0.02]
-        p-6
-        transition-all
-        duration-300
-        hover:border-cyan-500/40
-        hover:-translate-y-1.5
-hover:border-cyan-500/40
-hover:shadow-[0_0_25px_rgba(34,211,238,0.15)]
-        hover:bg-white/[0.04]
-      "
+    <BorderGlow
+    className="
+  block
+  h-full
+  rounded-2xl
+  p-6
+  transition-transform
+  duration-300
+  hover:-translate-y-1
+"
+      edgeSensitivity={30}
+      glowColor="40 80 80"
+      backgroundColor="#120F17"
+      borderRadius={24}
+      glowRadius={35}
+      glowIntensity={1}
+      coneSpread={25}
+      animated={false}
+      colors={["#c084fc", "#06b6d4", "#38bdf8"]}
     >
-      <div className="flex flex-col items-center text-center">
-        <Image
-  src={icon}
-  alt={name}
-  width={56}
-  height={56}
-  className="
-    object-contain
-    transition-transform
-    duration-300
-    group-hover:scale-110
-transition-all
-duration-300
-  "
-/>
+      {/* <Link
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+      > */}
+        <div className="flex flex-col items-center text-center">
+          <Image
+            src={icon}
+            alt={name}
+            width={56}
+            height={56}
+            className="
+              object-contain
+              transition-all
+              duration-300
+              group-hover:scale-110
+            "
+          />
 
-        <h3 className="mt-5 text-xl font-semibold">
-          {name}
-        </h3>
+          <h3 className="mt-5 text-xl font-semibold">
+            {name}
+          </h3>
 
-        <p className="mt-2 text-sm text-gray-400">
-          {category}
-        </p>
-      </div>
-    </Link>
+          <p className="mt-2 text-sm text-gray-400">
+            {category}
+          </p>
+        </div>
+      {/* </Link> */}
+    </BorderGlow>
   );
 }
